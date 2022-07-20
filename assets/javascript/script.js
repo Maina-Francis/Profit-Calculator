a3c605d2af0c7bb4f9562103b6f31434266f9dd2
Seb <sebastianfrancix@gmail.com>
2022-07-20 04:42:24 +0300

    adds the daily production output js

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -10,3 +10,42 @@ function makeReport() {
 
 var totalData = data[0] + data[1] + data[2];
 var totalProduction = data[0] + data[1] + data[2] + data[3];
+
+// Daily Production output
+
+document.getElementById("output-data").innerHTML += "<br>";
+
+document.getElementById("output-data").innerHTML +=
+  "<p>Your production for Shed A is" +
+  " " +
+  data[0] +
+  " " +
+  "Litres per day</p>";
+
+document.getElementById("output-data").innerHTML +=
+  "<p>Your production for Shed B is" +
+  " " +
+  data[1] +
+  " " +
+  "Litres per day</p>";
+
+document.getElementById("output-data").innerHTML +=
+  "<p>Your production for Shed C is" +
+  " " +
+  data[2] +
+  " " +
+  "Litres per day</p>";
+
+document.getElementById("output-data").innerHTML +=
+  "<p>Your pruduction for Shed D is" +
+  " " +
+  data[3] +
+  " " +
+  "Litres per day</p>";
+
+document.getElementById("output-data").innerHTML +=
+  "<p>The total production is" +
+  " " +
+  tdProduction +
+  " " +
+  "Litres per day</p>";

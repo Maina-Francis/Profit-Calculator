8ce608ad7aaaad412c698fc4a187ff96d5ee4c40
Seb <sebastianfrancix@gmail.com>
2022-07-20 03:16:58 +0300

    adds the makeReport function

---
+++ assets/javascript/script.js
@@ -0,0 +1,7 @@
+function makeReport() {
+  let data = [];
+  data[0] = parseInt(document.getElementById("shed-a").value);
+  data[1] = parseInt(document.getElementById("shed-b").value);
+  data[2] = parseInt(document.getElementById("shed-c").value);
+  data[3] = parseInt(document.getElementById("shed-d").value);
+}

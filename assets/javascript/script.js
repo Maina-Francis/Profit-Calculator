4f4de7955ccc8cce573d56f607d034fd9cdc0897
Seb <sebastianfrancix@gmail.com>
2022-07-20 03:23:06 +0300

    adds the total data and totalProduction var

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,3 +1,5 @@
+// make report function
+
 function makeReport() {
   let data = [];
   data[0] = parseInt(document.getElementById("shed-a").value);
@@ -5,3 +7,6 @@ function makeReport() {
   data[2] = parseInt(document.getElementById("shed-c").value);
   data[3] = parseInt(document.getElementById("shed-d").value);
 }
+
+var totalData = data[0] + data[1] + data[2];
+var totalProduction = data[0] + data[1] + data[2] + data[3];
